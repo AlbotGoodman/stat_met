dbe9a161d2c7d1f306cb2fa35eaa7bd780ce5d0f
Anders Sanderli <anders.sanderli@iths.se>
2025-01-28 14:15:12 +0100

    [LAB] reviewed variable names and calculations

--- lab/linear_regression.py
+++ lab/linear_regression.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.stats as stats
 
+
 class LinearRegression:
 
     def __init__(self):
@@ -16,7 +17,7 @@ class LinearRegression:
             y: Response variable
         """
         X = np.column_stack((np.ones(len(X)), X))
-        self.b = np.linalg.inv(X.T @ X) @ X.T @ y
+        self.b = np.linalg.pinv(X.T @ X) @ X.T @ y
     
     def predict(self, X):
         """
@@ -25,8 +26,9 @@ class LinearRegression:
         Args:
             X: Design matrix
         """
-        y_hat = X @ self.b
-        return y_hat
+        X = np.column_stack((np.ones(len(X)), X))
+        self.y_hat = X @ self.b
+        return self.y_hat
 
     def evaluate(self, X, y):
         """
@@ -36,26 +38,30 @@ class LinearRegression:
             X: Design matrix
             y: Response variable
         """
-        
-        k = len(self.b)-1
+        X = np.column_stack((np.ones(len(X)), X))
+        d = len(self.b)-1
         n = y.shape[0]
         SSE = np.sum(np.square(y - self.y_hat))
-        variance = SSE / (n - k - 1)
-        std_err = np.sqrt(self.variance)
-        Syy = (n * np.sum(np.square(y)) - np.square(np.sum(y)))/n
-        SSR = Syy - SSE
-        R_squared = SSR / Syy
-        F_stat = (SSR / k) / std_err
-        F_pvalue = stats.f.sf(F_stat, k, n-k-1)
-        cov_matrix = np.linalg.pinv(X.T @ X) * variance
-        RMSE = np.sqrt((1/(n-2))*SSE)
-        t_stat = [self.b[i] / (std_err * np.sqrt(cov_matrix[i, i])) for i in range(k)] # holds t-statistics for each coefficient
-        p_values = [2 * min(stats.t.cdf(i, n-k-1), stats.t.sf(i, n-k-1)) for i in t_stat] # holds p-values for each coefficient
+        var = SSE / (n - d - 1)
+        std_dev = np.sqrt(var)
+        SST = np.sum(np.square(y - np.mean(y)))
+        SSR = SST - SSE
+        R_squared = SSR / SST
+        F_stat = (SSR / d) / var
+        F_pvalue = stats.f.sf(F_stat, d, n-d-1)
+        cov_matrix = np.linalg.pinv(X.T @ X) * var
+        MSE = (1 / n) * SSE
+        RMSE = np.sqrt(MSE)
+        t_stat = [self.b[i] / (std_dev * np.sqrt(cov_matrix[i, i])) for i in range(d)] # holds t-statistics for each coefficient
+        p_values = [2 * min(stats.t.cdf(i, n-d-1), stats.t.sf(i, n-d-1)) for i in t_stat] # holds p-values for each coefficient
         return {
+            "Variance": var,
+            "Standard deviation": std_dev,
+            "SST": SST,
             "R_squared": R_squared,
+            "MSE": MSE,
             "RMSE": RMSE,
             "F_stat": F_stat,
             "F_pvalue": F_pvalue,
-            "t_stat": t_stat,
             "p_values": p_values
         }
