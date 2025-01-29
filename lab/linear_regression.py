a12758ac66da7c5e6ffa7b626bc0fa0fb1b1a61c
Anders Sanderli <anders.sanderli@iths.se>
2025-01-29 18:06:20 +0100

    [LAB] revamped the class

--- lab/linear_regression.py
+++ lab/linear_regression.py
@@ -5,74 +5,84 @@ import scipy.stats as stats
 class LinearRegression:
 
     def __init__(self):
+        self._d = None
+        self._n = None
         self.b = None
         self.y_hat = None
+
+    @property
+    def d(self):
+        return self._d
     
+    @property
+    def n(self):
+        return self._n
+
+    @property
+    def confidence_level(self):
+        pass
+
+    def variance(self, X, y):
+        SSE = np.sum(np.square(y - self.y_hat))
+        return SSE / (self._n - self._d - 1)
+
+    def standard_deviation(self, X, y):
+        var = self.variance(X, y)
+        return np.sqrt(var)
+
+    def significance(self, X, y):
+        var = self.variance(X, y)
+        std_dev = np.sqrt(var)
+        SSE = np.sum(np.square(y - self.y_hat))
+        SST = np.sum(np.square(y - np.mean(y)))
+        SSR = SST - SSE
+        f_stat = (SSR / self._d) / var
+        f_pvalue = stats.f.sf(f_stat, self._d, self._n - self._d - 1)
+        cov_matrix = np.linalg.pinv(X.T @ X) * var
+        ti_stat = [self.b[i] / (std_dev * np.sqrt(cov_matrix[i, i])) for i in range(self._d)]
+        ti_pvalues = [2 * min(stats.t.cdf(i, self._n - self._d - 1), stats.t.sf(i, self._n - self._d - 1)) for i in ti_stat]
+        return {
+            "f_pvalue": f_pvalue, 
+            "ti_pvalues": ti_pvalues
+        }
+
+    def relevance(self, X, y):
+        SSE = np.sum(np.square(y - self.y_hat))
+        SST = np.sum(np.square(y - np.mean(y)))
+        SSR = SST - SSE
+        return SSR / SST
+    
+    def test_relevance(self, X, y):                     # vilken n är det verkligen vi använder här??
+        SSE = np.sum(np.square(y - self.y_hat))
+        RSE = np.sqrt((1 / (self._n - 2)) * SSE)
+        MSE = (1 / self._n) * SSE
+        RMSE = np.sqrt(MSE)
+        return {
+            "RSE": RSE,
+            "MSE": MSE, 
+            "RMSE": RMSE
+        }
+
+    def pearson(self, X, y):
+        kin_geo = stats.pearsonr(X[:,1], X[:,2])
+        kin_ine = stats.pearsonr(X[:,1], X[:,3])
+        geo_ine = stats.pearsonr(X[:,2], X[:,3])
+        return {
+            "kin_geo": kin_geo,
+            "kin_ine": kin_ine,
+            "geo_ine": geo_ine
+        }
+
+    def confidence(self):
+        pass
+
     def fit(self, X, y):
-        """
-        Training the model.
-        
-        Args:
-            X: Design matrix
-            y: Response variable
-        """
         X = np.column_stack((np.ones(len(X)), X))
-        self.b = np.linalg.pinv(X.T @ X) @ X.T @ y      # an Ordinary Least Squares method
-    
+        self.b = np.linalg.pinv(X.T @ X) @ X.T @ y
+        self._d = len(self.b) - 1
+        self._n = y.shape[0]
+
     def predict(self, X):
-        """
-        Use the model on new data to make predictions. 
-        
-        Args:
-            X: Design matrix
-        """
         X = np.column_stack((np.ones(len(X)), X))
         self.y_hat = X @ self.b
         return self.y_hat
-
-    def evaluate(self, X, y):
-        """ 
-        Assessing the model. 
-        
-        Args: 
-            X: Design matrix 
-            y: Response variable 
-        """
-        X = np.column_stack((np.ones(len(X)), X))
-        d = len(self.b)-1                               # number of parameters/dimensions/features
-        n = y.shape[0]
-        SSE = np.sum(np.square(y - self.y_hat))
-        var = SSE / (n - d - 1)
-        std_dev = np.sqrt(var)
-        SST = np.sum(np.square(y - np.mean(y)))
-        SSR = SST - SSE
-        R_squared = SSR / SST                           # indicates how much of the data is explained by your model (0 to 1)
-        r = stats.pearsonr(y, self.y_hat)[0]
-        f_stat = (SSR / d) / var
-        f_pvalue = stats.f.sf(f_stat, d, n-d-1)         # tests significance of all parameters at once
-        cov_matrix = np.linalg.pinv(X.T @ X) * var
-        ti_stat = [self.b[i] / (std_dev * np.sqrt(cov_matrix[i, i])) for i in range(d)] # holds t-statistics for each coefficient
-        ti_pvalues = [2 * min(stats.t.cdf(i, n-d-1), stats.t.sf(i, n-d-1)) for i in ti_stat] # holds p-values for each coefficient
-        r0 = stats.pearsonr(X[:,2], X[:,3])             # do this over and over again for each pair of features
-        RSE = np.sqrt((1 / (n - 2)) * SSE)
-        MSE = (1 / n) * SSE                             # values closer to zero indicate that the model’s predictions are closer to the actual values
-        RMSE = np.sqrt(MSE)
-        return {
-            "Coefficients": self.b,
-            "Variance": var,
-            "Standard deviation": std_dev,
-            "SSE": SSE,
-            "SST": SST,
-            "SSR": SSR,
-            "R_squared": R_squared,
-            "r": r,
-            "MSE": MSE,
-            "RMSE": RMSE,
-            "RSE": RSE,
-            "F_stat": f_stat,
-            "F_pvalue": f_pvalue,
-            # "cov_matrix": cov_matrix,
-            # "ti_stat": ti_stat,
-            "ti_pvalues": ti_pvalues,
-            "r0": r0
-        }
