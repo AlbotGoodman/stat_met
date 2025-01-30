e9aefe3d39e79d23a5fdf3fb46fdce73583f7682
Anders Sanderli <anders.sanderli@iths.se>
2025-01-30 19:55:49 +0100

    [LAB] added Pearson to the class and fixed some bugs

--- lab/linear_regression.py
+++ lab/linear_regression.py
@@ -7,8 +7,8 @@ class LinearRegression:
     def __init__(self):
         self._d = None
         self._n = None
+        self._con_lvl = 0.95
         self.b = None
-        self.y_hat = None
 
     @property
     def d(self):
@@ -20,10 +20,18 @@ class LinearRegression:
 
     @property
     def confidence_level(self):
-        pass
+        return self._con_lvl
+
+    def fit(self, X, y):
+        self.b = np.linalg.pinv(X.T @ X) @ X.T @ y
+        self._d = len(self.b) - 1
+        self._n = y.shape[0]
+
+    def predict(self, X):
+        return X @ self.b
 
     def variance(self, X, y):
-        SSE = np.sum(np.square(y - self.y_hat))
+        SSE = np.sum(np.square(y - X @ self.b))
         return SSE / (self._n - self._d - 1)
 
     def standard_deviation(self, X, y):
@@ -33,7 +41,7 @@ class LinearRegression:
     def significance(self, X, y):
         var = self.variance(X, y)
         std_dev = np.sqrt(var)
-        SSE = np.sum(np.square(y - self.y_hat))
+        SSE = np.sum(np.square(y - X @ self.b))
         SST = np.sum(np.square(y - np.mean(y)))
         SSR = SST - SSE
         f_stat = (SSR / self._d) / var
@@ -47,13 +55,14 @@ class LinearRegression:
         }
 
     def relevance(self, X, y):
-        SSE = np.sum(np.square(y - self.y_hat))
+        SSE = np.sum(np.square(y - X @ self.b))
         SST = np.sum(np.square(y - np.mean(y)))
         SSR = SST - SSE
-        return SSR / SST
+        R_squared = SSR / SST
+        return R_squared
     
-    def test_relevance(self, X, y):                     # vilken n är det verkligen vi använder här??
-        SSE = np.sum(np.square(y - self.y_hat))
+    def test_relevance(self, X, y):
+        SSE = np.sum(np.square(y - X @ self.b))
         RSE = np.sqrt((1 / (self._n - 2)) * SSE)
         MSE = (1 / self._n) * SSE
         RMSE = np.sqrt(MSE)
@@ -72,17 +81,3 @@ class LinearRegression:
             "kin_ine": kin_ine,
             "geo_ine": geo_ine
         }
-
-    def confidence(self):
-        pass
-
-    def fit(self, X, y):
-        X = np.column_stack((np.ones(len(X)), X))
-        self.b = np.linalg.pinv(X.T @ X) @ X.T @ y
-        self._d = len(self.b) - 1
-        self._n = y.shape[0]
-
-    def predict(self, X):
-        X = np.column_stack((np.ones(len(X)), X))
-        self.y_hat = X @ self.b
-        return self.y_hat
