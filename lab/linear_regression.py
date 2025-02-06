3b3308549ddcb32774368a0f279d9a770b7d10c7
Anders Sanderli <anders.sanderli@iths.se>
2025-02-06 09:52:32 +0100

    [LAB] minor improvements and updated the to do list

--- lab/linear_regression.py
+++ lab/linear_regression.py
@@ -28,7 +28,7 @@ class LinearRegression:
         self._d = len(self.b) - 1
         self._n = y.shape[0]
 
-    def predict(self, X):
+    def predict(self, X):                                               # val
         return X @ self.b
 
     def variance(self, X, y):
@@ -55,13 +55,13 @@ class LinearRegression:
             "ti_pvalues": ti_pvalues
         }
 
-    def r_squared(self, X, y):
+    def r_squared(self, X, y):                                          # val
         SSE = np.sum(np.square(y - X @ self.b))
         SST = np.sum(np.square(y - np.mean(y)))
         SSR = SST - SSE
         return SSR / SST
     
-    def test_relevance(self, X, y):
+    def relevance(self, X, y):                                          # val
         SSE = np.sum(np.square(y - X @ self.b))
         RSE = np.sqrt(SSE / (self._n - 2))
         MSE = (1 / self._n) * SSE
@@ -79,12 +79,12 @@ class LinearRegression:
         var = self.variance(X, y)
         std_dev = self.standard_deviation(X, y)
         cov_matrix = np.linalg.pinv(X.T @ X) * var
-        t_crit = stats.t.ppf(self.a / 2, self._n - self._d - 1)
+        t_crit = stats.t.ppf(1 - self.a / 2, self._n - self._d - 1)
         ci = []
         for i in range(self._d + 1):
             std_err = std_dev * np.sqrt(cov_matrix[i,i])
             margin = t_crit * std_err
-            lower = self.b[i] - margin
-            upper = self.b[i] + margin
-            ci.append((lower, upper))
+            # lower = self.b[i] - margin
+            # upper = self.b[i] + margin
+            ci.append((margin))
         return ci
