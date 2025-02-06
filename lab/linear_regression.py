90bfc72f5810756882a79b4daf3971355c689073
Anders Sanderli <anders.sanderli@iths.se>
2025-02-06 15:48:32 +0100

    [LAB] rewrote the class with helper methods - in line with DRY

--- lab/linear_regression.py
+++ lab/linear_regression.py
@@ -7,9 +7,8 @@ class LinearRegression:
     def __init__(self):
         self._d = None
         self._n = None
-        self.con_lvl = 0.997       # pga R²
-        self.a = 1-self.con_lvl    # alpha
-        self.b = None
+        self._con_lvl = 0.95
+        self._b = None
 
     @property
     def d(self):
@@ -21,34 +20,45 @@ class LinearRegression:
 
     @property
     def confidence_level(self):
-        return self.con_lvl
-
+        return self._con_lvl
+    
+    @property
+    def alpha(self):
+        return 1 - self._con_lvl
+    
     def fit(self, X, y):
-        self.b = np.linalg.pinv(X.T @ X) @ X.T @ y
-        self._d = len(self.b) - 1
+        self._b = np.linalg.pinv(X.T @ X) @ X.T @ y
+        self._d = len(self._b) - 1
         self._n = y.shape[0]
 
     def predict(self, X):                                               # val
-        return X @ self.b
+        return X @ self._b
 
-    def variance(self, X, y):
-        SSE = np.sum(np.square(y - X @ self.b))
-        return SSE / (self._n - self._d - 1)
+    def _errors(self, X, y):
+        return y - self.predict(X)
 
-    def standard_deviation(self, X, y):
-        var = self.variance(X, y)
-        return np.sqrt(var)
+    def _sse(self, X, y):
+        return np.sum(np.square(self._errors(X, y)))
+    
+    def _sst(self, y):
+        return np.sum(np.square(y - np.mean(y)))
+    
+    def _ssr(self, X, y):
+        return self._sst(y) - self._sse(X, y)
+
+    def _variance(self, X, y):
+        return self._sse(X, y) / (self._n - self._d - 1)
+
+    def _standard_deviation(self, X, y):
+        return np.sqrt(self._variance(X, y))
+    
+    def _cov_matrix(self, X, y):
+        return np.linalg.pinv(X.T @ X) * self._variance(X, y)
 
     def significance(self, X, y):
-        var = self.variance(X, y)
-        std_dev = np.sqrt(var)
-        SSE = np.sum(np.square(y - X @ self.b))
-        SST = np.sum(np.square(y - np.mean(y)))
-        SSR = SST - SSE
-        f_stat = (SSR / self._d) / var
+        f_stat = (self._ssr(X, y) / self._d) / self._variance(X, y)
         f_pvalue = stats.f.sf(f_stat, self._d, self._n - self._d - 1)
-        cov_matrix = np.linalg.pinv(X.T @ X) * var
-        ti_stat = [self.b[i] / (std_dev * np.sqrt(cov_matrix[i, i])) for i in range(self._d)]
+        ti_stat = [self._b[i] / (self._standard_deviation(X, y) * np.sqrt(self._cov_matrix(X, y)[i, i])) for i in range(self._d)]
         ti_pvalues = [2 * min(stats.t.cdf(i, self._n - self._d - 1), stats.t.sf(i, self._n - self._d - 1)) for i in ti_stat]
         return {
             "f_pvalue": f_pvalue, 
@@ -56,15 +66,11 @@ class LinearRegression:
         }
 
     def r_squared(self, X, y):                                          # val
-        SSE = np.sum(np.square(y - X @ self.b))
-        SST = np.sum(np.square(y - np.mean(y)))
-        SSR = SST - SSE
-        return SSR / SST
+        return self._ssr(X, y) / self._sst(y)
     
     def relevance(self, X, y):                                          # val
-        SSE = np.sum(np.square(y - X @ self.b))
-        RSE = np.sqrt(SSE / (self._n - 2))
-        MSE = (1 / self._n) * SSE
+        RSE = np.sqrt(self._sse(X, y) / (self._n - 2))
+        MSE = (1 / self._n) * self._sse(X, y)
         RMSE = np.sqrt(MSE)
         return {
             "RSE": RSE,
@@ -77,13 +83,9 @@ class LinearRegression:
         return np.corrcoef(r_X, rowvar=False)
     
     def confidence_intervals(self, X, y):
-        var = self.variance(X, y)
-        std_dev = self.standard_deviation(X, y)
-        cov_matrix = np.linalg.pinv(X.T @ X) * var
-        t_crit = stats.t.ppf(1 - self.a / 2, self._n - self._d - 1)
+        t_crit = stats.t.ppf(1 - self.alpha / 2, self._n - self._d - 1)
         ci = []
         for i in range(self._d + 1):
-            std_err = std_dev * np.sqrt(cov_matrix[i,i])
-            margin = t_crit * std_err
+            margin = t_crit * self._standard_deviation(X, y) * np.sqrt(self._cov_matrix(X, y)[i,i])
             ci.append(margin)
         return ci
